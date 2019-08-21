ce6f6ffb91c23707654a252c2bc167dda38d316a
Tony Wasserka <neobrain@users.noreply.github.com>
2019-08-21 16:38:58 +0200

    Restructure storage interfaces
    Storages may now support bidirectional seeking, and they may opt to support only input or only output operations.

--- include/blobify/storage_backend.hpp
+++ include/blobify/storage_backend.hpp
@@ -8,20 +8,38 @@ namespace blob {
 /**
  * Defines read/write primitives for interacting with the underlying data source/target
  */
-struct storage_backend {
-    void skip(std::size_t num_bytes);
+struct storage_base {
+    /**
+     * Seeks the stream from the current position
+     * @note Implementations that implement both input_storage and output_storage are not required to keep separate read/write cursors
+     * @pre num_bytes may not exceed the lower storage bound
+     */
+    void seek(std::ptrdiff_t num_bytes);
+};
 
+struct input_storage : storage_base {
+
+    /**
+     * @post The read cursor is advanced by num_bytes elements as if by calling seek(num_bytes)
+     * @note Should throw storage_exhausted_exception on error
+     */
     void load(std::byte* target, std::size_t num_bytes);
+};
+
+struct output_storage : storage_base {
+    /**
+     * @post The write cursor is advanced by num_bytes elements as if by calling seek(num_bytes)
+     * @note Should throw storage_exhausted_exception on error
+     */
     void store(std::byte* source, std::size_t num_bytes);
 
-    // TODO: StorageRemaining() for efficient bounds checks?
 };
 
 namespace detail {
 
 // Type-erased abstraction for a runtime-provided backend
 // TODO: Also buffer on fixed-size array first
-struct default_storage_backend : storage_backend {
+struct default_storage_backend {
     // TODO
 };
 
