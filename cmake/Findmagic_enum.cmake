bf1fd9c3efa9f713d381ca38aeb824149e7f5b8f
Tony Wasserka <neobrain@users.noreply.github.com>
2020-08-09 15:24:56 +0200

    Fix typo in Findmagic_enum script

--- cmake/Findmagic_enum.cmake
+++ cmake/Findmagic_enum.cmake
@@ -7,7 +7,7 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(magic_enum
     REQUIRED_VARS MAGIC_ENUM_INCLUDE_DIR)
 
-if(PFR_FOUND AND NOT TARGET magic_enum::magic_enum)
+if(magic_enum_FOUND AND NOT TARGET magic_enum::magic_enum)
     add_library(magic_enum::magic_enum INTERFACE IMPORTED)
     set_target_properties(magic_enum::magic_enum PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${MAGIC_ENUM_INCLUDE_DIR}")
